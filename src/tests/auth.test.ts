f34254a626b9d79a36ca1e14452a3c95213a3b35
shaikrahim04 <theshaikrahim@gmail.com>
2025-08-15 15:02:45 +0000

    chore: unfix ci test to test the test

---
+++ src/tests/auth.test.ts
@@ -0,0 +1,20 @@
+import { describe, expect, test } from "vitest";
+
+const person = {
+  isActive: false,
+  age: 32,
+};
+
+describe("person", () => {
+  test("person is defined", () => {
+    expect(person).toBeDefined();
+  });
+
+  test("is active", () => {
+    expect(person.isActive).toBeTruthy();
+  });
+
+  test("is minor", () => {
+    expect(person.age).toBeLessThanOrEqual(50);
+  });
+});
